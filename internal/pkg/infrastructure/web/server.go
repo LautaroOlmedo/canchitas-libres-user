beae3c60a9bd212c41ad3bf8e418ad3176f3c4ca
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 <lautaroolmedo77@gmail.com>
2025-06-07 19:35:54 -0300

    added whitCORS function

--- internal/pkg/infrastructure/web/server.go
+++ internal/pkg/infrastructure/web/server.go
@@ -22,9 +22,27 @@ func NewServer(configuration *configuration.Configuration, handler *Handler) (*S
 func (s *Server) Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/user/", s.handler.ServeHTTP)
+	handlerWithCORS := withCORS(mux)
+
 	fmt.Printf("Server listening on %s%s\n", s.config.SERVER.DOMAIN, s.config.SERVER.SERVER_PORT)
-	err := http.ListenAndServe(s.config.SERVER.SERVER_PORT, mux)
+	err := http.ListenAndServe(s.config.SERVER.SERVER_PORT, handlerWithCORS)
 	if err != nil {
 		log.Fatalf("Failed to start server: %s \n", err)
 	}
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Encabezados CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
